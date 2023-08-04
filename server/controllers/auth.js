4b9857c05f480f1d3416e3090b47fca0810d08ea
Owais Ahmad <owaisahmad014@gmail.com>
2023-08-04 22:53:44 +0500

    ..

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,25 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+
+/* Register User */
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt()
+    } catch (err) {
+
+    }
+}
