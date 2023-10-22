128dbe57481839222ac7e6aa006a577aa07e7576
Owais Ahmad <owaisahmad014@gmail.com>
2023-10-22 19:58:43 +0500

    now working on implemneting post controller, route

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js"
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/createPosts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
 
@@ -45,10 +48,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
+app.user("/posts", postRoutes);
 
 
 /* MONGOOSE SETUP */
