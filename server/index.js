f02f8a8e80a3167a3ce1e828caa4a7e2283a37a3
Owais Ahmad <owaisahmad014@gmail.com>
2023-11-20 21:45:31 +0500

    added mock data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
 
 
 
