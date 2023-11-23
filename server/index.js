03255eee90f79a44dc614c57c4cf811fcd2d1898
Owais Ahmad <owaisahmad014@gmail.com>
2023-11-23 21:12:12 +0500

    backend fully done

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 
 
@@ -63,9 +65,17 @@ const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`now server is running and Server port is: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
+})
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`now server is running and Server port is: ${PORT}`));
+        //ADD DATA ONE TIME
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+
+    })
+    .catch((error) =>
+        console.log(`${error} did not connect`));
 
 
 
