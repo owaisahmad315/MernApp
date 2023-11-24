0d2288fcd9721071bf632acba106f96924722269
Owais Ahmad <owaisahmad014@gmail.com>
2023-11-24 11:00:09 +0500

    frontttt commiitttt

---
+++ server/routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRomoveFriends,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/friendId", verifyToken, addRomoveFriends);
+
+export default router;
+
